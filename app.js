52b2644beb8cb6fe75c1340d09bf8c99bc6d7aa1
Viet Tran <tranhuyviet@gmail.com>
2019-12-20 17:30:17 +0200

    handle simple get request API: /api/v1/tours

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    // res.status(200).send('Hello from server side!');
-    res.status(200).json({ message: 'Hello from server side!', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//     res.status(200).json({ message: 'Hello from server side!', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint...');
+// app.post('/', (req, res) => {
+//     res.send('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours: tours
+        }
+    });
 });
 
 const port = 3000;

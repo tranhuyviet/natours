52a018a01c85d9b3b3ee69d63dc5ae46161e1f6a
Viet Tran <tranhuyviet@gmail.com>
2019-12-21 13:25:04 +0200

    added config.env and setup it

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // MIDDLEWARE
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 

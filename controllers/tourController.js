eb178876197406fe8b3e082a64f4ab15321d3e7d
Viet Tran <tranhuyviet@gmail.com>
2019-12-21 12:23:10 +0200

    65 chaining multi middleware: checkBody and createTour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -15,6 +15,18 @@ exports.checkId = (req, res, next, val) => {
     next();
 };
 
+// created middleware to check body
+exports.checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Missing name or price'
+        });
+    }
+
+    next();
+};
+
 // handle tours
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -8,10 +8,15 @@ const tourController = require('./../controllers/tourController');
 
 router.param('id', tourController.checkId);
 
+// Create a checkBody middleware
+// Check if body contains the name and price property
+// If not, send back 400 (bad request)
+// Add it to the post handler stack
+
 router
     .route('/')
     .get(tourController.getAllTours)
-    .post(tourController.createTour);
+    .post(tourController.checkBody, tourController.createTour);
 
 router
     .route('/:id')

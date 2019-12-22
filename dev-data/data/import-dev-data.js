16bc63907ea5a68acf236042c2d4dd0bcb3e9424
Viet Tran <tranhuyviet@gmail.com>
2019-12-22 15:15:54 +0200

    use small script to import data from json file to the server and delete all data from the server

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+    // use local db
+    // .connect(process.env.DATABASE_LOCAL, {
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        // console.log(con.connections);
+        console.log('DB connection successful!');
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+// READ JSON FILE
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+// IMPORT DATA INTO DATABASE
+const importData = async () => {
+    try {
+        await Tour.create(tours);
+        console.log('Data successfully loaded!');
+    } catch (err) {
+        console.log(err);
+    }
+    process.exit();
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+    try {
+        await Tour.deleteMany();
+        console.log('Data successfully deleted!');
+    } catch (err) {
+        console.log(err);
+    }
+    process.exit();
+};
+
+console.log(process.argv);
+
+if (process.argv[2] === '--import') {
+    importData();
+} else if (process.argv[2] === '--delete') {
+    deleteData();
+}

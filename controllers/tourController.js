c77385179869ce2615f38e52faef57cd605fa82f
Viet Tran <tranhuyviet@gmail.com>
2019-12-22 15:51:18 +0200

    created the query and execute it by basic

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,32 @@ const Tour = require('./../models/tourModel');
 // handle tours
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find();
+        // BUILD THE QUERY
+        const queryObj = { ...req.query };
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+
+        excludedFields.forEach(el => delete queryObj[el]);
+
+        console.log(req.query, queryObj);
+
+        // way 1 to filter:
+        // const tours = await Tour.find({
+        //     duration: 5,
+        //     difficulty: 'easy'
+        // });
+
+        // do by way 1:
+        const query = Tour.find(queryObj);
+
+        // way 2:
+        // const query = Tour.find()
+        //     .where('duration')
+        //     .equals(5)
+        //     .where('difficulty')
+        //     .equals('easy');
+
+        // EXECUTE THE QUERY
+        const tours = await query;
 
         res.status(200).json({
             status: 'success',

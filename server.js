1ab67260512fad18e00bc56aeba18eec1b193eee
Viet Tran <tranhuyviet@gmail.com>
2019-12-22 10:51:55 +0200

    config env file and setup basic mongoose and connect to database

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+    // use local db
+    // .connect(process.env.DATABASE_LOCAL, {
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        // console.log(con.connections);
+        console.log('DB connection successful!');
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 const app = require('./app');
 

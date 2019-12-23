176189c53e0b31991ae6749c47232947760c8aa3
Viet Tran <tranhuyviet@gmail.com>
2019-12-23 14:50:38 +0200

    added monthly-plan

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -142,7 +142,7 @@ exports.getTourStats = async (req, res) => {
                 $sort: { avgPrice: 1 }
             }
 
-            // {
+            // ,{
             //     $match: { _id: { $ne: 'EASY' } } // $ne = not equal
             // }
         ]);
@@ -151,6 +151,59 @@ exports.getTourStats = async (req, res) => {
             status: 'success',
             data: {
                 stats
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+    try {
+        const year = req.params.year * 1;
+        // console.log(year);
+        const plan = await Tour.aggregate([
+            {
+                $unwind: '$startDates'
+            },
+            {
+                $match: {
+                    startDates: {
+                        $gte: new Date(`${year}-01-01`),
+                        $lte: new Date(`${year}-12-31`)
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: { $month: '$startDates' },
+                    numTourStarts: { $sum: 1 },
+                    tours: { $push: '$name' }
+                }
+            },
+            {
+                $addFields: { month: '$_id' }
+            },
+            {
+                $project: {
+                    _id: 0 // not show the _id; 1 is showed
+                }
+            },
+            {
+                $sort: { numTourStarts: -1 }
+            },
+            {
+                $limit: 12
+            }
+        ]);
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                plan
             }
         });
     } catch (err) {

b45fbfc9a99a3d7a1729a7988ff7360091d797c6
Viet Tran <tranhuyviet@gmail.com>
2019-12-25 11:35:49 +0200

    added middleware handling error

--- app.js
+++ app.js
@@ -28,12 +28,27 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter); // add tour router in to middleware (mounting)
 app.use('/api/v1/users', userRouter); // add user router in to middleware (mounting)
 
-// handle if url request not correctly
+// handle if url request not correctly or not defined
 // .all that mean every request: get, post, update, delete,...
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on this server!`
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Can't find ${req.originalUrl} on this server!`
+    // });
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+    err.status = 'fail';
+    err.statusCode = 404;
+    next(err);
+});
+
+// middleware handling error
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500; // 500 is internal server error
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
     });
 });
 

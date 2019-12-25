4343f24131841e08306ee73106837e988cb8beb0
Viet Tran <tranhuyviet@gmail.com>
2019-12-25 11:51:16 +0200

    added AppError class and added errorController

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -35,21 +38,16 @@ app.all('*', (req, res, next) => {
     //     status: 'fail',
     //     message: `Can't find ${req.originalUrl} on this server!`
     // });
-    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-    err.status = 'fail';
-    err.statusCode = 404;
-    next(err);
+
+    // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+    // err.status = 'fail';
+    // err.statusCode = 404;
+    // next(err);
+
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 // middleware handling error
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500; // 500 is internal server error
-    err.status = err.status || 'error';
-
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;

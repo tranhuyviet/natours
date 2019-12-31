4cc42e319e52623d4365e00d0238f8bc99d51621
Viet Tran <tranhuyviet@gmail.com>
2019-12-31 12:57:40 +0200

    added helmet to security HTTP headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -11,9 +12,13 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1. GLOBAL MIDDLEWARE
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+    app.use('MORGAN', morgan('dev'));
 }
 
 // limit 100 request on 1 hour with 1 IP
@@ -24,18 +29,16 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter); // only limit with /api... request
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-    console.log('Hello from the middleware 👋');
-    next();
-});
-
+// Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     // console.log(req.headers);
-
     next();
 });
 

11de5a754f227e7f3d1f8713ae901569ce0e3896
Viet Tran <tranhuyviet@gmail.com>
2020-01-02 14:00:19 +0200

    added review model

---
+++ models/reviewModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+// const validator = require('validator');
+
+const reviewSchema = new mongoose.Schema(
+    {
+        review: {
+            type: String,
+            required: [true, 'Review can not be empty']
+        },
+        rating: {
+            type: Number,
+            min: [1, 'Rating must have to lager then 1.0'],
+            max: [5, 'Rating must have to less then 5.0']
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now()
+        },
+        tour: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Tour',
+            required: [true, 'Review must belong to a tour.']
+        },
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User',
+            required: [true, 'Review must belong to a user.']
+        }
+    },
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
+    }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;

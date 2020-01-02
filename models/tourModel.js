eadaf3a64b1dd149b8e7f7459b93677177d25265
Viet Tran <tranhuyviet@gmail.com>
2020-01-02 11:00:20 +0200

    used mongoose.Schema.ObjectId to reference User collection

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 // const validator = require('validator');
-const User = require('./userModel');
+// const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
     {
@@ -112,7 +112,12 @@ const tourSchema = new mongoose.Schema(
                 day: Number
             }
         ],
-        guides: Array
+        guides: [
+            {
+                type: mongoose.Schema.ObjectId,
+                ref: 'User'
+            }
+        ]
     },
     {
         toJSON: { virtuals: true },
@@ -131,11 +136,11 @@ tourSchema.pre('save', function(next) {
     next();
 });
 
-tourSchema.pre('save', async function(next) {
-    const guidesPromises = this.guides.map(async id => await User.findById(id));
-    this.guides = await Promise.all(guidesPromises);
-    next();
-});
+// tourSchema.pre('save', async function(next) {
+//     const guidesPromises = this.guides.map(async id => await User.findById(id));
+//     this.guides = await Promise.all(guidesPromises);
+//     next();
+// });
 
 // tourSchema.pre('save', function(next) {
 //     console.log('Will save document...');

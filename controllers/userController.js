923292df65d01729676e4195ad5f922c391f9206
Viet Tran <tranhuyviet@gmail.com>
2020-01-03 15:38:50 +0200

    added middleware getMe in userController and setup /me in router

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,6 +25,11 @@ const filterObj = (obj, ...allowedFields) => {
 
 exports.getAllUsers = factory.getAll(User);
 
+exports.getMe = (req, res, next) => {
+    req.params.id = req.user.id;
+    next();
+};
+
 exports.updateMe = catchAsync(async (req, res, next) => {
     // 1. Create error if user POST password data
     if (req.body.password || req.body.passwordConfirm)

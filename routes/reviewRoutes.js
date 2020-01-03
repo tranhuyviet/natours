14d406947be45b8db414963be39b737f2f6e95fb
Viet Tran <tranhuyviet@gmail.com>
2020-01-03 11:20:32 +0200

    adding nested get all reviews on tour

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -5,6 +5,8 @@ const router = express.Router({ mergeParams: true });
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
+// POST /tours/tourId/reviews : post review in The tour (tourId)
+// GET /tours/tourId/reviews : get all revies in The tour (tourId)
 router
     .route('/')
     .get(reviewController.getAllReviews)

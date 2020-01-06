6d775031e2acbab4fd15ae45941f7dbcedbca6e1
Viet Tran <tranhuyviet@gmail.com>
2020-01-06 11:26:45 +0200

    added viewRoutes and viewController

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -72,25 +73,8 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
-app.get('/', (req, res) => {
-    res.status(200).render('base', {
-        tour: 'The Forest Hiker',
-        user: 'Viet'
-    });
-});
-
-app.get('/overview', (req, res) => {
-    res.status(200).render('overview', {
-        title: 'All Tours'
-    });
-});
-
-app.get('/tour', (req, res) => {
-    res.status(200).render('tour', {
-        title: 'The Forest Hiker Tour'
-    });
-});
-
+// View routes
+app.use('/', viewRouter);
 // API routes
 app.use('/api/v1/tours', tourRouter); // add tour router in to middleware (mounting)
 app.use('/api/v1/users', userRouter); // add user router in to middleware (mounting)
---
+++ routes/viewRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+const viewController = require('./../controllers/viewController');
+
+router.get('/', viewController.getOverview);
+
+router.get('/tour', viewController.getTour);
+
+module.exports = router;

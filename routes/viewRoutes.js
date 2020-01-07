fb3c34ad5d97fcbdf8abe2df30ddafc748827eb1
Viet Tran <tranhuyviet@gmail.com>
2020-01-07 19:26:05 +0200

    completed part1

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,10 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 const viewController = require('./../controllers/viewController');
+const authController = require('./../controllers/authController');
 
 router.get('/', viewController.getOverview);
 
-router.get('/tour/:slug', viewController.getTour);
+router.get('/tour/:slug', authController.protect, viewController.getTour);
 
 router.get('/login', viewController.getLoginForm);
 

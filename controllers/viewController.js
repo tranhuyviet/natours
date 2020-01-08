624a9dd89d7cc9c4b003bf87323103e05843da2f
Viet Tran <tranhuyviet@gmail.com>
2020-01-08 14:08:18 +0200

    implemented handler when rendering error pages

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,6 @@
 const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
     // 1. Get tour data from collection
@@ -21,6 +22,9 @@ exports.getTour = catchAsync(async (req, res, next) => {
         fields: 'review rating user'
     });
     // console.log(tour);
+
+    if (!tour)
+        return next(new AppError('There is not tour with that name.', 404));
 
     // 2. Build template
 

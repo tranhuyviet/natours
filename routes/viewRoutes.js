55ecf00709bee7b7231a6176d4598e2c412c057b
Viet Tran <tranhuyviet@gmail.com>
2020-01-10 16:34:54 +0200

    completed added booking on db

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,14 +4,17 @@ const router = express.Router();
 
 const viewController = require('./../controllers/viewController');
 const authController = require('./../controllers/authController');
+const bookingController = require('./../controllers/bookingController');
 
+router.get(
+    '/',
+    bookingController.createBookingCheckout,
+    authController.isLoggedIn,
+    viewController.getOverview
+);
+router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
+router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
 router.get('/me', authController.protect, viewController.getAccount);
-
-router.use(authController.isLoggedIn);
-
-router.get('/', viewController.getOverview);
-router.get('/tour/:slug', viewController.getTour);
-router.get('/login', viewController.getLoginForm);
 
 router.post(
     '/submit-user-data',
